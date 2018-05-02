6087071ba3a372ef789bd8c10e67b7529a18cdb5
hwase0ng <hwase0ng@users.noreply.github.com>
2018-05-03 00:13:25 +0800

    Completion of scraping KLSE listing

--- scrapers/wsj/scrapeStocksListing.py
+++ scrapers/wsj/scrapeStocksListing.py
@@ -7,15 +7,14 @@ Created on May 2, 2018
 import settings as S
 import requests
 from BeautifulSoup import BeautifulSoup
-from Utils.dateutils import getToday
 from Utils.fileutils import getStockCode
 
 WSJSTOCKSURL = 'https://quotes.wsj.com/company-list/country/malaysia'
 
 
-def connectStocksListing():
+def connectStocksListing(url):
     global soup
-    stocksListingUrl = WSJSTOCKSURL
+    stocksListingUrl = url
     try:
         page = requests.get(stocksListingUrl, headers=S.HEADERS)
         assert(page.status_code == 200)
@@ -37,28 +36,47 @@ def scrapeStocksListing(soup):
     # for each row, there are many rows including no table
     for tr in table.findAll('tr'):
         td = tr.findAll('td')
+        if len(td) == 0:
+            continue
         # Sample stockLink: <a href="https://quotes.wsj.com/MY/XKLS/SEM">
         stockLink = tr.find('a').get('href')
         stockShortName = stockLink[31:]
-        stockName = td.findAll('span', {'class': 'cl-name'}).upper()
-        stockCode = getStockCode(stockShortName, '../i3investor/klse.txt')
+        stockName = tr.find('span', {'class': 'cl-name'}).text.upper().replace('AMP;', '')
+        stockCode = getStockCode(stockShortName, '../i3investor/klse.txt', './klse.wsj')
         if len(stockCode) == 0:
-            print "ERR:Unmatched stock:", stockShortName, stockName
+            print "ERR:Unmatched stock:", stockShortName, ',', stockName
         tds = [x.text.strip() for x in td]
         xchange = tds[1]
         sector = tds[2]
+        if len(sector) == 0:
+            sector = '-'
         if S.DBG_ALL:
             print stockShortName, stockCode, stockName, xchange, sector
         stocks[stockShortName] = [stockCode, stockName, xchange, sector]
-    return stocks
+
+    nextpg = getNextPage(soup)
+    return stocks, nextpg
 
 
 def unpackListing(scode, sname, xchange, sector):
     return scode, sname, xchange, sector
 
 
+def getNextPage(soup):
+    pages = soup.find("div", "nav-right")
+    li = pages.find("li", "next")
+    nextpg = li.a.get('href')
+    if nextpg == '#':
+        return None
+    return nextpg
+
+
 def writeStocksListing(outfile='klse.txt'):
-    stocksListing = scrapeStocksListing(connectStocksListing())
+    stocksListing = {}
+    nextpg = WSJSTOCKSURL
+    while(nextpg is not None):
+        listing, nextpg = scrapeStocksListing(connectStocksListing(nextpg))
+        stocksListing.update(listing)
 
     fh = open(outfile, "w")
     for key in sorted(stocksListing.iterkeys()):
