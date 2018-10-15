4196fa6279f2acdceaffa34e5a0278694ee13ece
roysten918 <roysten918@users.noreply.github.com>
2018-10-16 00:51:45 +0800

    Added down motion statistic (kind of redundant)

--- analytics/mvp.py
+++ analytics/mvp.py
@@ -1,4 +1,13 @@
 '''
+Usage: main [-pwh] [COUNTER] ...
+
+Arguments:
+    COUNTER           Optional counters
+Options:
+    -p,--portfolio    Select portfolio from config.json
+    -w,--watchlist    Select watchlist from config.json
+    -h,--help         This page
+
 Created on Oct 14, 2018
 
 Motion - 12 days up in 15 days
@@ -8,7 +17,8 @@ Price  - 20% up in 15 days
 @author: hwase0ng
 '''
 
-from common import loadCfg, formStocklist, FifoDict
+from common import getCounters, loadCfg, formStocklist, FifoDict
+from docopt import docopt
 import csv
 import settings as S
 import traceback
@@ -18,23 +28,26 @@ def unpackEOD(counter, dt, price_open, price_high, price_low, price_close, volum
     return counter, dt, price_open, price_high, price_low, price_close, volume
 
 
-def readCSV(counter):
+def generateMVP(counter, stkcode):
     totalVol = 0.0
     totalPrice = 0.0
-    daysUp = 0
+    mvpDaysUp = 0
+    mvpDaysDown = 0
     eodlist = FifoDict()
     for i in range(S.MVP_DAYS):
         # counter, date, open, high, low, close, volume, total vol, total price,
-        #   up/down from previous day, days up, vol diff, price diff
-        eodlist.append(['', '1900-01-{:02d}'.format(i), 0, 0, 0, 0, 1.0, 1.0, 0.0001, 0, 0, 0.0, 0.0])
-    lasteod = ['', '1900-01-14'.format(i), 0, 0, 0, 0, 1.0, 1.0, 0.0001, 0, 0, 0.0, 0.0]
+        #   down from previous day, up from previous day,
+        #   total days down, total days up, vol diff, price diff
+        eodlist.append(['', '1900-01-{:02d}'.format(i), 0, 0, 0, 0, 1.0, 1.0, 0.0001, 0, 0, 0, 0, 0.0, 0.0])
+    lasteod = ['', '1900-01-14'.format(i), 0, 0, 0, 0, 1.0, 1.0, 0.0001, 0, 0, 0, 0, 0.0, 0.0]
 
     S.DBG_ALL = False
     if S.DBG_ALL:
         for _ in range(S.MVP_DAYS):
             print eodlist.pop()
 
-    with open(counter, "rb") as fl:
+    fh = open(S.DATA_DIR + 'MVP/mvp-' + counter + '.csv', "w")
+    with open(S.DATA_DIR + counter + '.' + stkcode + '.csv', "rb") as fl:
         try:
             reader = csv.reader(fl, delimiter=',')
             for i, line in enumerate(reader):
@@ -44,34 +57,42 @@ def readCSV(counter):
                         i, stock, dt, popen, phigh, plow, pclose, volume)
                 if pclose >= popen and pclose >= lasteod[5]:
                     dayUp = 1
+                    dayDown = 0
                 else:
                     dayUp = 0
+                    dayDown = -1
                 eodpop = eodlist.pop()
-                daysUp = daysUp + dayUp - int(eodpop[-4])
-                totalVol = totalVol + float(volume) - float(eodpop[6])
+                mvpDaysUp = mvpDaysUp + dayUp - int(eodpop[-5])
+                mvpDaysDown = mvpDaysDown + dayDown - int(eodpop[-6])
                 totalPrice = totalPrice + float(pclose) - float(eodpop[5])
+                totalVol = totalVol + float(volume) - float(eodpop[6])
                 aveVol = float(eodpop[7]) / S.MVP_DAYS
                 avePrice = float(eodpop[8]) / S.MVP_DAYS
                 volDiff = (float(volume) - aveVol) / aveVol
                 priceDiff = (float(pclose) - avePrice) / avePrice
+                priceDiff *= 20  # easier to view as value is below 1
                 if S.DBG_ALL and dt.startswith('2018-07'):
                     print '\t', dt, aveVol, avePrice, volDiff, priceDiff
-                neweod = '{},{},{},{},{},{},{},{},{:.2f},{},{},{:.2f},{:.2f}'.format(
-                    stock, dt, popen, phigh, plow, pclose, volume,
-                    totalVol, totalPrice, dayUp, daysUp, volDiff, priceDiff)
-                print neweod
+                neweod = '{},{},{},{},{},{},{},{},{:.2f},{},{},{},{},{:.2f},{:.2f}'.format(
+                    stock, dt, popen, phigh, plow, pclose, volume, totalVol, totalPrice,
+                    dayDown, dayUp, mvpDaysDown, mvpDaysUp, volDiff, priceDiff)
+                if S.DBG_ALL:
+                    print neweod
+                fh.write(neweod + '\n')
                 eodlist.append(neweod.split(','))
                 lasteod = line
         except Exception:
             print eodpop
             traceback.print_exc()
+    fh.close()
 
 
 if __name__ == '__main__':
+    args = docopt(__doc__)
     cfg = loadCfg(S.DATA_DIR)
     global klse
     klse = "scrapers/i3investor/klse.txt"
-    stocks = 'DUFU'
+    stocks = getCounters(args['COUNTER'], args['--portfolio'], args['--watchlist'], False)
     stocklist = formStocklist(stocks, klse)
     for shortname in sorted(stocklist.iterkeys()):
-        readCSV(S.DATA_DIR + shortname + '.' + stocklist[shortname] + '.csv')
+        generateMVP(shortname, stocklist[shortname])
