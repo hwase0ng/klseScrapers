d967cc64e3171bd5b145b19de78d42ed1155f8ba
roysten918 <roysten918@users.noreply.github.com>
2018-10-17 22:04:36 +0800

    Subplot and added annotation

--- analytics/mvpchart.py
+++ analytics/mvpchart.py
@@ -15,10 +15,16 @@ Created on Oct 16, 2018
 '''
 
 from common import getCounters, loadCfg, formStocklist, loadKlseCounters
+from utils.dateutils import getDaysBtwnDates
 from docopt import docopt
 from pandas import read_csv
 from matplotlib import pyplot as plt
 import settings as S
+
+
+def getMpvDate(dfdate):
+    mpvdt = str(dfdate.to_pydatetime()).split()
+    return mpvdt[0]
 
 
 def mvpChart(counter):
@@ -30,22 +36,71 @@ def mvpChart(counter):
                              'total vol', 'total price', 'dayB4 motion', 'M', 'P', 'V'],
                       usecols=['date', 'close', 'M', 'P', 'V'])
         # df.set_index('Date', drop=True, inplace=True)
-        mvpdate = df.iloc[-1]['date']
-        mvpdt = str(mvpdate.to_pydatetime()).split()
+        mpvdate = getMpvDate(df.iloc[-1]['date'])
+        idxM = df.index[df['M'] > 10]
+        idxV = df.index[df['V'] > 24]
+        firstidx = df.index.get_loc(df.iloc[chartDays].name)
         if S.DBG_ALL:
             print(df.tail(10))
-            print type(mvpdate), mvpdate
-            print type(mvpdt), mvpdt
+            print type(mpvdate), mpvdate
+            print idxV[-5:]
+            print df.index.get_loc(df.iloc[chartDays].name)
+
         axes = df[chartDays:].plot(x='date', figsize=(15, 7), subplots=True, grid=False,
-                                   title=mvpdt[0] + ': MPV Chart of ' + counter)
+                                   title=mpvdate + ': MPV Chart of ' + counter)
         ax1 = plt.gca().axes.get_xaxis()
         axlabel = ax1.get_label()
         axlabel.set_visible(False)
-        axes[1].axhline(10, color='k')
-        axes[2].axhline(0, color='k')
-        axes[3].axhline(25, color='k')
-        # plt.show()
-        plt.savefig(fname + ".png")
+        axes[1].axhline(10, color='k', linestyle='--')
+        axes[2].axhline(0, color='k', linestyle='--')
+        axes[3].axhline(25, color='k', linestyle='--')
+
+        group_motion = []
+        for i in range(1, len(idxM)):
+            j = i * -1
+            if idxM[j] < firstidx:
+                break
+            mpvdate = getMpvDate(df.iloc[idxM[j]]['date'])
+            motion = df.iloc[idxM[j]]['M']
+            if S.DBG_ALL:
+                print j, mpvdate, motion
+            if i + 1 < len(idxM):
+                next_mpvdate = getMpvDate(df.iloc[idxM[j - 1]]['date'])
+                if getDaysBtwnDates(next_mpvdate, mpvdate) < 5:
+                    group_motion.append([mpvdate[5:], str(motion)])
+                    continue
+                else:
+                    group_motion.append([mpvdate[5:], str(motion)])
+            else:
+                group_motion.append([mpvdate[5:], str(motion)])
+
+            strMotion = ""
+            group_motion.reverse()
+            for k in range(len(group_motion)):
+                strMotion += "> " + "<".join(group_motion[k])
+                if (k + 1) < len(group_motion) and (k + 1) % 3 == 0:
+                    strMotion += ">\n"
+            group_motion = []
+            strMotion = strMotion[2:] + ">"
+            xyx = mpvdate[:5] + strMotion[-10:-4]
+            xyy = int(strMotion[-3:-1])
+            axes[1].annotate(strMotion, size=8, xycoords='data', xy=(xyx, xyy),
+                             xytext=(10, 10), textcoords='offset points',
+                             arrowprops=dict(arrowstyle='-|>'))
+        for i in range(1, len(idxV)):
+            j = i * -1
+            if idxV[j] < firstidx:
+                break
+            mpvdate = getMpvDate(df.iloc[idxV[j]]['date'])
+            vol = df.iloc[idxV[j]]['V']
+            if S.DBG_ALL:
+                print j, mpvdate, vol
+            axes[3].annotate(mpvdate[5:] + "(" + str(vol) + ")",
+                             xycoords='data', xy=(mpvdate, vol),
+                             xytext=(10, 10), textcoords='offset points',
+                             arrowprops=dict(arrowstyle='-|>'))
+        plt.show()
+        # plt.savefig(fname + ".png")
         plt.close()
 
 
