5492bd1178c97e95e437aebfc629d9ea83105bd8
hwase0ng <hwase0ng@users.noreply.github.com>
2018-11-17 23:55:55 +0800

    New collectionComposition to share results for top patterns

--- analytics/mvpsignals.py
+++ analytics/mvpsignals.py
@@ -23,9 +23,13 @@ def scanSignals(dbg, counter, pnlist, lastTrxnData):
     if len(pnlist) < 3:
         print "Skipped len:", counter, len(pnlist)
         return False
+
+    cmpvlists, composelist = collectCompositions(pnlist, lastTrxnData)
+    if cmpvlists is None:
+        return ""
     bbprice, rvsM, rvsP, rvsV, bottomrevs, oversold, oversold_stage = \
-        bottomBuySignals(pnlist, lastTrxnData)
-    topSell = topSellSignals(bbprice, pnlist)
+        bottomBuySignals(lastTrxnData, cmpvlists, composelist)
+    topSell = topSellSignals(bbprice, pnlist, composelist)
     if not oversold and not oversold_stage and not bottomrevs and not topSell:
         # if (bbprice < 0 or rvsM < 0 or rvsP < 0):
         if not dbg:
@@ -56,80 +60,61 @@ def scanSignals(dbg, counter, pnlist, lastTrxnData):
     return signals
 
 
-def topSellSignals(pricepos, pnlist):
+def topSellSignals(pricepos, pnlist, composelist):
     topSellSignal = 0
     if pricepos < 4:
         return topSellSignal
-    pnM = pnlist[2]
-    xpM, xnM, ypM, ynM = pnM[0], pnM[1], pnM[2], pnM[3]  # 0=XP, 1=XN, 2=YP, 3=YN
-    ynmC, ypmC, ypmM, ypmV = ynM[0], ypM[0], ypM[1], ypM[3]  # 0=C, 1=M, 2=P, 3=V
-    if ypmC is None or ynmC is None:
-        # if len(ypmM) > 1 and ypmM[-1] < ypmM[-2]:
-        #     topSellSignal = 1
-        return topSellSignal
-    xnmC, xpmC, xpmM, xpmV = xnM[0], xpM[0], xpM[1], xpM[3]  # 0=C, 1=M, 2=P, 3=V
-    minYN, maxYP = min(ynmC), max(ypmC)
-    xpmCdate = xpmC[ypmC.index(maxYP)]
-    xnmCdate = xnmC[ynmC.index(minYN)]
-    if ypmM is not None and len(ypmM) > 1 and ypmM[-1] < ypmM[-2]:
+    composeC, composeM, composeP, composeV = \
+        composelist[0], composelist[1], composelist[2], composelist[3]
+    [posC, newlowC, newhighC, topC, bottomC, prevtopC, prevbottomC, retrace] = composeC
+    [posM, newlowM, newhighM, topM, bottomM, prevtopM, prevbottomM, _] = composeM
+    [posP, newlowP, newhighP, topP, bottomP, prevtopP, prevbottomP, _] = composeP
+    [posV, newlowV, newhighV, topV, bottomV, prevtopV, prevbottomV, _] = composeV
+
+    if (newhighC or topC) and (newhighP or topP):
         topSellSignal = 1
-        if xpmCdate > xnmCdate:
+        if topC or topP:
             topSellSignal = 2
+
+    if not topSellSignal:
+        pnM = pnlist[2]
+        xpM, xnM, ypM, ynM = pnM[0], pnM[1], pnM[2], pnM[3]  # 0=XP, 1=XN, 2=YP, 3=YN
+        ynmC, ypmC, ypmM, ypmV = ynM[0], ypM[0], ypM[1], ypM[3]  # 0=C, 1=M, 2=P, 3=V
+        if ypmC is None or ynmC is None:
+            # if len(ypmM) > 1 and ypmM[-1] < ypmM[-2]:
+            #     topSellSignal = 1
+            return topSellSignal
+        xnmC, xpmC, xpmM, xpmV = xnM[0], xpM[0], xpM[1], xpM[3]  # 0=C, 1=M, 2=P, 3=V
+        minYN, maxYP = min(ynmC), max(ypmC)
+        xpmCdate = xpmC[ypmC.index(maxYP)]
+        xnmCdate = xnmC[ynmC.index(minYN)]
+        if ypmM is not None and len(ypmM) > 1 and ypmM[-1] < ypmM[-2]:
+            topSellSignal = 1
+            if xpmCdate > xnmCdate:
+                topSellSignal = 2
+
     return topSellSignal
 
 
-def formListCMPV(cmpv, pnlist):
-    xp, xn, yp, yn = pnlist[0], pnlist[1], pnlist[2], pnlist[3]  # 0=XP, 1=XN, 2=YP, 3=YN
-    # cmpv 0=C, 1=M, 2=P, 3=V
-    cmpvlist = []
-    cmpvlist.append(xp[cmpv])
-    cmpvlist.append(xn[cmpv])
-    cmpvlist.append(yp[cmpv])
-    cmpvlist.append(yn[cmpv])
-    return cmpvlist
-
-
-def bottomBuySignals(pnlist, lastTrxn):
+def bottomBuySignals(lastTrxn, cmpvlists, composelist):
     oversold, oversold_stage, bottomrevs = 0, 0, 0
-    # lastTrxnData = [lastTrxnDate, lastClosingPrice, lastTrxnC, lastTrxnM, lastTrxnP, lastTrxnV]
     lastprice, lastC, lastM, lastP, lastV = \
         lastTrxn[1], lastTrxn[2], lastTrxn[3], lastTrxn[4], lastTrxn[5]
-
-    pnW, pnF, pnM = pnlist[0], pnlist[1], pnlist[2]
-    cmpvWC = formListCMPV(0, pnW)
-    cmpvMC = formListCMPV(0, pnM)
-    cmpvMM = formListCMPV(1, pnM)
-    cmpvMP = formListCMPV(2, pnM)
-    cmpvMV = formListCMPV(3, pnM)
-    posC, newlowC, newhighC, retrace, topC, bottomC, prevtopC, prevbottomC = \
-        checkposition('C', cmpvMC + cmpvWC, lastC)
-    if DBGMODE:
-        print "lastPrice vs lastC:", lastprice, lastC
-        print "C=%d,l=%d,h=%d,t=%d,b=%d,pT=%d,pB=%d,r=%d" % \
-            (posC, newlowC, newhighC, topC, bottomC, prevtopC, prevbottomC, retrace)
-    if posC < 0:
-        return posC, -1, -1, -1, bottomrevs, oversold, oversold_stage
-    posM, newlowM, newhighM, _, topM, bottomM, prevtopM, prevbottomM = \
-        checkposition('M', cmpvMM, lastM)
-    posP, newlowP, newhighP, _, topP, bottomP, prevtopP, prevbottomP = \
-        checkposition('P', cmpvMP, lastP)
-    posV, newlowV, newhighV, _, topV, bottomV, prevtopV, prevbottomV = \
-        checkposition('V', cmpvMV, lastV)
-    if DBGMODE:
-        print "M=%d,l=%d,h=%d,t=%d,b=%d,pT=%d,pB=%d" % \
-            (posM, newlowM, newhighM, topM, bottomM, prevtopM, prevbottomM)
-        print "P=%d,l=%d,h=%d,t=%d,b=%d,pT=%d,pB=%d" % \
-            (posP, newlowP, newhighP, topP, bottomP, prevtopP, prevbottomP)
-        print "V=%d,l=%d,h=%d,t=%d,b=%d,pT=%d,pB=%d" % \
-            (posV, newlowV, newhighV, topV, bottomV, prevtopV, prevbottomV)
-
+    cmpvMC, cmpvMM, cmpvMP, cmpvMV = cmpvlists[0], cmpvlists[1], cmpvlists[2], cmpvlists[3]
     plistC, nlistC, nlistM, nlistP, plistV = \
         cmpvMC[2], cmpvMC[3], cmpvMM[3], cmpvMP[3], cmpvMV[2]  # 0=XP, 1=XN, 2=YP, 3=YN
+    composeC, composeM, composeP, composeV = \
+        composelist[0], composelist[1], composelist[2], composelist[3]
+    [posC, newlowC, newhighC, topC, bottomC, prevtopC, prevbottomC, retrace] = composeC
+    [posM, newlowM, newhighM, topM, bottomM, prevtopM, prevbottomM, _] = composeM
+    [posP, newlowP, newhighP, topP, bottomP, prevtopP, prevbottomP, _] = composeP
+    [posV, newlowV, newhighV, topV, bottomV, prevtopV, prevbottomV, _] = composeV
     '''
     1 - PADINI 2014-03 (LowC + LowM with higher P divergent) - short rebound
     2 - PADINI 2011-10-12 (LowC + LowP with higher M divergent) - reversal
     3 - PETRONM 2013-04-24: extension of 1 after retrace
     4 - EDGENTA 2018-08-16 with 30% rebound (LowC + highP)
+    5 - DUFU 2015-08-26 (BottomM + BottomP + BottomV) - strong reversal
     '''
     oversold = 0 if nlistM is None or nlistP is None or len(nlistM) < 2 or len(nlistP) < 2 \
         else 1 if (newlowC or bottomC) and (newlowM or bottomM) and min(nlistM) < 5 \
@@ -142,6 +127,7 @@ def bottomBuySignals(pnlist, lastTrxn):
         and min(nlistM) < 5 and nlistM[-1] > 5 and posC == 1 \
         else 4 if (newlowC or bottomC) and not (newlowM or bottomM) and not (newlowP or bottomP) \
         and (newhighP or topP or newhighM or topM) and not (prevtopM or prevtopP) \
+        else 5 if not (newlowC or bottomC) and bottomM and bottomP and bottomV \
         else 0
     if oversold:
         oversold_stage = 1
@@ -237,7 +223,7 @@ def checkposition(pntype, pnlist, lastpos):
             prevbottom = True
 
         if pntype == 'C':
-            if not bottom:
+            if not newhigh and not bottom:
                 range3 = (maxP - minN) / 3
                 if lastpos <= minN + range3:
                     pos = 1
@@ -259,7 +245,66 @@ def checkposition(pntype, pnlist, lastpos):
 
         retrace = True if (top or prevtop) and pos > 1 else False
 
-    return pos, newlow, newhigh, retrace, top, bottom, prevtop, prevbottom
+    return [pos, newlow, newhigh, top, bottom, prevtop, prevbottom, retrace]
+
+
+def formListCMPV(cmpv, pnlist):
+    xp, xn, yp, yn = pnlist[0], pnlist[1], pnlist[2], pnlist[3]  # 0=XP, 1=XN, 2=YP, 3=YN
+    # cmpv 0=C, 1=M, 2=P, 3=V
+    cmpvlist = []
+    cmpvlist.append(xp[cmpv])
+    cmpvlist.append(xn[cmpv])
+    cmpvlist.append(yp[cmpv])
+    cmpvlist.append(yn[cmpv])
+    return cmpvlist
+
+
+def collectCompositions(pnlist, lastTrxn):
+    oversold, oversold_stage, bottomrevs = 0, 0, 0
+    # lastTrxnData = [lastTrxnDate, lastClosingPrice, lastTrxnC, lastTrxnM, lastTrxnP, lastTrxnV]
+    lastprice, lastC, lastM, lastP, lastV = \
+        lastTrxn[1], lastTrxn[2], lastTrxn[3], lastTrxn[4], lastTrxn[5]
+
+    pnW, pnF, pnM = pnlist[0], pnlist[1], pnlist[2]
+    cmpvWC = formListCMPV(0, pnW)
+    cmpvMC = formListCMPV(0, pnM)
+    cmpvMM = formListCMPV(1, pnM)
+    cmpvMP = formListCMPV(2, pnM)
+    cmpvMV = formListCMPV(3, pnM)
+    composeC = checkposition('C', cmpvMC + cmpvWC, lastC)
+    if DBGMODE:
+        print "lastPrice vs lastC:", lastprice, lastC
+        [posC, newlowC, newhighC, topC, bottomC, prevtopC, prevbottomC, retrace] = composeC
+        print "C=%d,l=%d,h=%d,t=%d,b=%d,pT=%d,pB=%d,r=%d" % \
+            (posC, newlowC, newhighC, topC, bottomC, prevtopC, prevbottomC, retrace)
+    posC = composeC[0]
+    if posC < 0:
+        return None, None
+    composeM = checkposition('M', cmpvMM, lastM)
+    composeP = checkposition('P', cmpvMP, lastP)
+    composeV = checkposition('V', cmpvMV, lastV)
+    if DBGMODE:
+        [posM, newlowM, newhighM, topM, bottomM, prevtopM, prevbottomM, _] = composeM
+        print "M=%d,l=%d,h=%d,t=%d,b=%d,pT=%d,pB=%d" % \
+            (posM, newlowM, newhighM, topM, bottomM, prevtopM, prevbottomM)
+        [posP, newlowP, newhighP, topP, bottomP, prevtopP, prevbottomP, _] = composeP
+        print "P=%d,l=%d,h=%d,t=%d,b=%d,pT=%d,pB=%d" % \
+            (posP, newlowP, newhighP, topP, bottomP, prevtopP, prevbottomP)
+        [posV, newlowV, newhighV, topV, bottomV, prevtopV, prevbottomV, _] = composeV
+        print "V=%d,l=%d,h=%d,t=%d,b=%d,pT=%d,pB=%d" % \
+            (posV, newlowV, newhighV, topV, bottomV, prevtopV, prevbottomV)
+
+    cmpvlists = []
+    cmpvlists.append(cmpvMC)
+    cmpvlists.append(cmpvMM)
+    cmpvlists.append(cmpvMP)
+    cmpvlists.append(cmpvMV)
+    composelist = []
+    composelist.append(composeC)
+    composelist.append(composeM)
+    composelist.append(composeP)
+    composelist.append(composeV)
+    return cmpvlists, composelist
 
 
 if __name__ == '__main__':
