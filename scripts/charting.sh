1fa09a2ff3cfe4c65205fff7eca997d2a6af6ba3
roysten918 <roysten918@users.noreply.github.com>
2018-12-20 12:43:28 +0800

    Removed -p

--- scripts/charting.sh
+++ scripts/charting.sh
@@ -1,6 +1,6 @@
 counter=$1
 dates=$2
 DATA=$3
-opts="-po -c 150"
+opts="-o -c 150"
 python analytics/mvpchart.py $counter $opts -S $dates:130
 mv ${DATA}/mpv/simulation/synopsis/$counter_*.png ${DATA}/mpv/simulation/profiling/$counter/
