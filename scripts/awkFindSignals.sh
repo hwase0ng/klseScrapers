997ac3a5074a48fd767144471490ca14b082bb59
roysten918 <roysten918@users.noreply.github.com>
2019-01-01 15:29:53 +0800

    Fixed edge values with ()

--- scripts/awkFindSignals.sh
+++ scripts/awkFindSignals.sh
@@ -5,20 +5,25 @@ mpvdir=$datadir/mpv
 signaldir=$mpvdir/signals
 re='^[0-9]+$'
 
-cmpdiv=10   # 1,2,3=CP+CM,CP,CM in pdiv; 4,5,6=same in ndiv
-c=11
-m=12
-p=13
-v=14
-tripleM=15   # p3u=1,2,3 p3d=,4,5,6, n3u=2,5,7, n3d=3,6,8
-tripleP=16
-tripleV=17
-narrowC=18
-narrowM=19
-narrowP=20
-countP=21
-tripleBottoms=22
-tripleTops=23
+#        Nx   N^   Nv
+#  P^    1    2    3
+#  Pv    4    5    6
+#  Px    X    7    8
+# p3u=1,2,3 p3d=,4,5,6, n3u=2,5,7, n3d=3,6,8
+cmpdiv=14   # 1,2,3=CP+CM,CP,CM in pdiv; 4,5,6=same in ndiv
+c=15
+m=16
+p=17
+v=18
+tripleM=19
+tripleP=20
+tripleV=21
+narrowC=22
+narrowM=23
+narrowP=24
+countP=25
+tripleBottoms=26
+tripleTops=27
 
 signalfile=""
 val=0
@@ -69,6 +74,16 @@ do
    then
     echo "$val is not an integer number!"
     exit 2
+   else
+    if [ "$signal" -eq "14" ]
+    then
+     val=`echo "("$val`
+    else
+     if [ "$signal" -eq "27" ]
+     then
+      val=`echo ${val}")"`
+     fi
+    fi
    fi
    ;;
   V)
@@ -80,8 +95,7 @@ do
    fi
    ;;
   g)
-   SET=$OPTARG
-   SET=`echo ${SET} | tr '[:lower:]' '[:upper:]'`
+   SET=`echo ${OPTARG} | tr '[:lower:]' '[:upper:]'`
    group=$(eval echo "\$$SET")
    if [ -z "$group" ]
    then
@@ -98,7 +112,7 @@ done
 shift $((OPTIND-1))
 [ "${1:-}" = "--" ] && shift
 
-if [ -z "$signal" -o "$val" -eq 0 ]
+if [ -z "$signal" -o "$val" -eq "0" ]
 then
    echo "Usage: awkFindSignals.sh -sSvVcegd <signal name> <value> [counter|group] [equal] [datadir]" 1>&2
    exit 1
@@ -112,12 +126,12 @@ then
         then
 			if [ $useEqual -eq 1 ]
 			then
-				awk -F'[,.]' -v sn=$signal -v val=$val '{if ($sn == val) {print $0}}' $@
+				awk -F'[,.^]' -v sn=$signal -v val=$val '{if ($sn == val) {print $0}}' $@
 			else
-				awk -F'[,.]' -v sn=$signal -v val=$val '{if ($sn > val) {print $0}}' $@
+				awk -F'[,.^]' -v sn=$signal -v val=$val '{if ($sn > val) {print $0}}' $@
 			fi
 		else
-            awk -F'[,.]' -v sn=$signal -v sn2=${signal2} -v val=$val -v val2=$val2 '{if ($sn == val && $sn2 == val2) {print $0}}' $@
+            awk -F'[,.^]' -v sn=$signal -v sn2=${signal2} -v val=$val -v val2=$val2 '{if ($sn == val && $sn2 == val2) {print $0}}' $@
 		fi
 		exit 0
 	else
@@ -129,22 +143,23 @@ fi
 for counter in $group
 do
 	signalfile=${signaldir}/${counter}-signals.csv
+	echo $counter, $signal, $val, $signalfile
     if [ -f $signalfile ]
     then
 		if [ -z "${signal2}" -o "${val2}" -eq 0 ]
 		then
 			if [ $useEqual -eq 1 ]
 			then
-				awk -F'[,.]' -v sn=$signal -v val=$val '{if ($sn == val) {print $0}}' $signalfile
+				awk -F'[,.^]' -v sn=$signal -v val=$val '{if ($sn == val) {print $0}}' $signalfile
 			else
-				awk -F'[,.]' -v sn=$signal -v val=$val '{if ($sn > val) {print $0}}' $signalfile
+				awk -F'[,.^]' -v sn=$signal -v val=$val '{if ($sn > val) {print $0}}' $signalfile
 			fi
 		else
 			if [ $useEqual -eq 1 ]
 			then
-  				awk -F'[,.]' -v sn=$signal -v sn2=${signal2} -v val=$val -v val2=$val2 '{if ($sn == val && $sn2 == val2) {print $0}}' $signalfile
+  				awk -F'[,.^]' -v sn=$signal -v sn2=${signal2} -v val=$val -v val2=$val2 '{if ($sn == val && $sn2 == val2) {print $0}}' $signalfile
   			else
-  				awk -F'[,.]' -v sn=$signal -v sn2=${signal2} -v val=$val -v val2=$val2 '{if ($sn > val && $sn2 > val2) {print $0}}' $signalfile
+  				awk -F'[,.^]' -v sn=$signal -v sn2=${signal2} -v val=$val -v val2=$val2 '{if ($sn > val && $sn2 > val2) {print $0}}' $signalfile
   			fi
 		fi
     else
