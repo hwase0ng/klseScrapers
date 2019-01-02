3d845b49be7f61b77bf693701ecde6e1e13a8e7f
roysten918 <roysten918@users.noreply.github.com>
2019-01-02 19:13:29 +0800

    No stock code is required now

--- scripts/invAwkPatchEOD.sh
+++ scripts/invAwkPatchEOD.sh
@@ -1,5 +1,8 @@
 COUNTER=$1
-SCODE=$2
-INDATA=/c/git/klseScrapers/data
+SRCDIR=/c/git/klseScrapers
+INDATA=$SRCDIR/data
 OUTDATA=/z/data
+cd $OUTDATA
+SCODE=`ls $COUNTER.*.csv | awk -F. '{print $2}'`
+cd $SRCDIR
 awk -F "," '{printf("%s,%s,%.4f,%.4f,%.4f,%.4f,%ld\n",$1,$2,$3,$4,$5,$6,$7)}' ${INDATA}/investingcom/${COUNTER}.${SCODE}.csv > ${OUTDATA}/${COUNTER}.${SCODE}.csvtmp
