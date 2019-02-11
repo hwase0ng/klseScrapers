2f69385558c625d3ad1b67c6313ed3457d4ef07d
hwase0ng <hwase0ng@users.noreply.github.com>
2019-02-11 10:41:42 +0800

    J scripts

--- scripts/jmissing.sh
+++ scripts/jmissing.sh
@@ -1,17 +1,18 @@
-group="DANCO DUFU PADINI CARLSBG KESM KLSE F&N"
-for counter in $group
+if [ -z "$1" ]
+then
+	echo "Usage: jmissing.sh <counter>"
+	exit 1
+fi
+counter=`echo $1 | tr '[:lower:]' '[:upper:]'`
+echo $counter
+sfile="/z/data/mpv/${counter}.csv"
+for j in `tail -n +200 $sfile`
 do
- echo $counter
- sfile="/z/data/mpv/${counter}.csv"
- for j in `tail -n +200 $sfile`
- do
-  ddate=`echo $j | awk -F, '{print $2}'`
-  f="data/json/$counter.$ddate.json"
-  echo $f
-  if ! [ -e $f ]
-  then
-   python analytics/mvpchart.py $counter -psj 1 -S $ddate -c 500
-  fi
-  done
- done
-
+	ddate=`echo $j | awk -F, '{print $2}'`
+	f="data/json/$counter.$ddate.json"
+	echo $f
+	if ! [ -e $f ]
+	then
+		python analytics/mvpchart.py $counter -psj 1 -S $ddate -c 500
+	fi
+done
---
+++ scripts/jresume.sh
@@ -0,0 +1,22 @@
+if [ -z "$1" ]
+then
+	echo "Usage: jresume.sh <counter>"
+	exit 1
+fi
+counter=`echo $1 | tr '[:lower:]' '[:upper:]'`
+sfile="/z/data/mpv/${counter}.csv"
+sdate=`ls -l data/json/${counter}.201[89]*.json | tail -1 | awk '{print $NF}' | awk -F[/.] '{print $4}'`
+lnum=`grep -n ${sdate} ${sfile} | awk -F: '{print $1}'`
+lcount=`wc -l ${sfile} | awk '{print $1}'`
+tnum=`expr $lnum - $lcount`
+echo $counter, $sdate, $lcount, $lnum, $tnum
+for j in `tail -n $tnum $sfile`
+do
+	ddate=`echo $j | awk -F, '{print $2}'`
+	f="data/json/$counter.$ddate.json"
+	echo $f
+	if ! [ -e $f ]
+	then
+		python analytics/mvpchart.py $counter -psj 1 -S $ddate -c 500
+	fi
+done
