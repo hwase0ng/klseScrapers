b21993915212c64a1b636126d6c446117e6b132d
hwase0ng <hwase0ng@users.noreply.github.com>
2020-03-10 22:38:49 +0800

    app password

---
+++ smtp/gsetup.py
@@ -0,0 +1,14 @@
+'''
+Created on Mar 8, 2020
+
+@author: hwase
+'''
+import yagmail
+
+
+if __name__ == '__main__':
+    contents = ['This is the body, and here is just text http://somedomain/image.png',
+                'You can find an audio file attached.', '/local/path/song.mp3']
+    sender_email = "insider4trader@gmail.com"
+    # yagmail.register(sender_email, 'vwxaotmoawdfwxzx')  # trader@2020
+    yagmail.SMTP(sender_email).send('roysten.tan@gmail.com', 'test', contents)
